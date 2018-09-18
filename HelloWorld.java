52a6b2176ba4c224c2f5b965acd069404feb7ef8
vincentalcindor <vincent.alcindor@gmail.com>
2018-09-18 10:15:20 +0100

    Updated in Ubuntu

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,9 @@ public class HelloWorld {
 	public static void main(String[] args){
 
 	System.out.println("Hello World");
-	System.out.println("Changed in GIT");	
+	System.out.println("Changed in GIT");
+	System.out.println("Changed in Ubuntu");		
+
 
 	}
 }
